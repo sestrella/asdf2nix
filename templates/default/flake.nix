34f0b20a4cb94677d8fe2f7ced12a6cf16a39d2d
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-12 13:28:23 -0500

    Rename asdf-python input

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -3,12 +3,12 @@
     # TODO: Change to GH url
     asdf2nix.url = "path:../..";
     # TODO: Change to GH url
-    asdf-python.url = "path:../../plugins/python";
+    asdf2nix-python.url = "path:../../plugins/python";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, asdf2nix, asdf-python, flake-utils, nixpkgs }:
+  outputs = { self, asdf2nix, asdf2nix-python, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -16,7 +16,7 @@
           inherit system;
           versionsFile = ./.tool-versions;
           plugins = {
-            python = asdf-python.lib.packageFromVersion;
+            python = asdf2nix-python.lib.packageFromVersion;
           };
         };
       in
