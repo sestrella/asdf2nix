c035e8d725bda716568f980863f8b9ec227dcee2
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-12 01:44:05 -0500

    Minor refactoring

--- flake.nix
+++ flake.nix
@@ -8,49 +8,51 @@
       }:
       let
         fileLines = file: builtins.split "\n" (builtins.readFile file);
-        mkVersion = rawVersion:
-          let
-            pluginAndVersion = builtins.split " " rawVersion;
-          in
-          {
-            name = builtins.elemAt pluginAndVersion 0;
-            value = builtins.elemAt pluginAndVersion 2;
-          };
-        mkPackage = plugin: version: plugins.${plugin} { inherit system version; };
-        checkPlugin = { name, ... }:
-          let
-            hasPlugin = builtins.hasAttr name plugins;
-          in
-          if skipMissingPlugins
-          then builtins.traceVerbose (if hasPlugin then "Plugin ${name} found" else "Skipping plugin ${name}") hasPlugin
-          else hasPlugin || throw ''
-            No plugin found for "${name}", try adding the missing plugin:
+        parseVersions = builtins.map
+          (line:
+            let
+              pluginAndVersion = builtins.split " " line;
+            in
+            {
+              name = builtins.elemAt pluginAndVersion 0;
+              value = builtins.elemAt pluginAndVersion 2;
+            });
+        filterPlugins = builtins.filter
+          ({ name, ... }:
+            let
+              hasPlugin = builtins.hasAttr name plugins;
+            in
+            if skipMissingPlugins
+            then builtins.traceVerbose (if hasPlugin then "Plugin ${name} found" else "Skipping plugin ${name}") hasPlugin
+            else hasPlugin || throw ''
+              No plugin found for "${name}", try adding the missing plugin:
 
-            ```
-            asdf2nix.lib.packagesFromToolVersions = {
-              plugins = {
-                ${name} = asdf-${name}.lib.packageFromVersion;
+              ```
+              asdf2nix.lib.packagesFromToolVersions = {
+                plugins = {
+                  ${name} = asdf-${name}.lib.packageFromVersion;
+                  ...
+                };
                 ...
               };
-              ...
-            };
-            ```
+              ```
 
-            Or enable `skipMissingPlugins` to skip this error:
+              Or enable `skipMissingPlugins` to skip this error:
 
-            ```
-            asdf2nix.lib.packagesFromToolVersions = {
-              plugins = { ... };
-              skipMissingPlugins = true;
-              ...
-            };
-            ```
-          '';
+              ```
+              asdf2nix.lib.packagesFromToolVersions = {
+                plugins = { ... };
+                skipMissingPlugins = true;
+                ...
+              };
+              ```
+            '');
+        findPackage = plugin: version: plugins.${plugin} { inherit system version; };
       in
-      builtins.mapAttrs mkPackage
+      builtins.mapAttrs findPackage
         (builtins.listToAttrs
-          (builtins.filter checkPlugin
-            (builtins.map mkVersion
+          (filterPlugins
+            (parseVersions
               (builtins.filter (x: x != [ ] && x != "")
                 (builtins.split "\n"
                   (builtins.readFile versionsFile))))));
