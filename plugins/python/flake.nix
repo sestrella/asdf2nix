23360bbb21464e7d3fe6f0a22b54582dc0d34087
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-14 06:42:01 -0500

    Add hasVersion to python plugin

--- plugins/python/flake.nix
+++ plugins/python/flake.nix
@@ -2,7 +2,11 @@
   inputs.nixpkgs-python.url = "github:cachix/nixpkgs-python";
 
   outputs = { self, nixpkgs-python }: {
-    lib.packageFromVersion = { system ? builtins.currentSystem, version }:
-      nixpkgs-python.packages.${system}.${version};
+    lib = {
+      hasVersion = { version, system ? builtins.currentSystem }:
+        builtins.hasAttr version nixpkgs-python.packages.${system};
+      packageFromVersion = { version, system ? builtins.currentSystem }:
+        nixpkgs-python.packages.${system}.${version};
+    };
   };
 }
