810fa572a59fc94debdd5f79e35d5cea1eb19225
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-21 00:50:50 -0500

    Setup semantic-release (#3)
    * Setup release workflow
    
    * Update status badge
    
    * Move release workflow to the right directory

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # asdf2nix
 
-[![CI](https://github.com/sestrella/asdf2nix/actions/workflows/ci.yml/badge.svg)](https://github.com/sestrella/asdf2nix/actions/workflows/ci.yml)
+[![Build](https://github.com/sestrella/asdf2nix/actions/workflows/build.yml/badge.svg)](https://github.com/sestrella/asdf2nix/actions/workflows/build.yml)
+[![Release](https://github.com/sestrella/asdf2nix/actions/workflows/release.yml/badge.svg)](https://github.com/sestrella/asdf2nix/actions/workflows/release.yml)
 
 asdf2nix is a tool designed for [asdf] users who want to experiment with
 nix-shells, as well as former Nix users who want to simplify package version
--- flake.nix
+++ flake.nix
@@ -1,97 +1,113 @@
 {
-  inputs.nixpkgs-lib.url = "github:nixos/nixpkgs/nixos-23.05?dir=lib";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  };
 
-  outputs = { self, nixpkgs-lib }:
-    let
-      lib = nixpkgs-lib.lib;
-    in
-    {
-      lib.packagesFromVersionsFile =
-        { versionsFile
-        , system ? builtins.currentSystem
-        , plugins ? { }
-        , skipMissingPlugins ? false
-        }:
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
         let
-          fileLines = file: lib.splitString "\n" (lib.fileContents file);
-          removeComments = builtins.filter (line: !lib.hasPrefix "#" line);
-          parseVersions = builtins.map
-            (line:
-              let
-                pluginAndVersion = lib.splitString " " line;
-              in
-              {
-                name = builtins.elemAt pluginAndVersion 0;
-                version = builtins.elemAt pluginAndVersion 1;
-              });
-          filterPlugins = builtins.filter
-            ({ name, ... }:
-              let
-                hasPlugin = builtins.hasAttr name plugins;
-              in
-              lib.throwIf (!skipMissingPlugins && !hasPlugin)
-                ''
-                  No plugin found for "${name}", try adding the missing plugin:
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ pkgs.nodejs ];
+          };
+        })
+    //
+    (
+      let
+        lib = nixpkgs.lib;
+      in
+      {
+        lib.packagesFromVersionsFile =
+          { versionsFile
+          , system ? builtins.currentSystem
+          , plugins ? { }
+          , skipMissingPlugins ? false
+          }:
+          let
+            fileLines = file: lib.splitString "\n" (lib.fileContents file);
+            removeComments = builtins.filter (line: !lib.hasPrefix "#" line);
+            parseVersions = builtins.map
+              (line:
+                let
+                  pluginAndVersion = lib.splitString " " line;
+                in
+                {
+                  name = builtins.elemAt pluginAndVersion 0;
+                  version = builtins.elemAt pluginAndVersion 1;
+                });
+            filterPlugins = builtins.filter
+              ({ name, ... }:
+                let
+                  hasPlugin = builtins.hasAttr name plugins;
+                in
+                lib.throwIf (!skipMissingPlugins && !hasPlugin)
+                  ''
+                    No plugin found for "${name}", try adding the missing plugin:
 
-                  ```
-                  <asdf2nix>.lib.packagesFromVersionsFile {
-                    plugins = {
-                      ${name} = <asdf2nix-${name}>.lib.packageFromVersion;
+                    ```
+                    <asdf2nix>.lib.packagesFromVersionsFile {
+                      plugins = {
+                        ${name} = <asdf2nix-${name}>.lib.packageFromVersion;
+                        ...
+                      };
                       ...
                     };
-                    ...
-                  };
-                  ```
+                    ```
 
-                  Or enable `skipMissingPlugins` to skip this error:
+                    Or enable `skipMissingPlugins` to skip this error:
 
-                  ```
-                  <asdf2nix>.lib.packagesFromVersionsFile {
-                    plugins = { ... };
-                    skipMissingPlugins = true;
-                    ...
-                  };
-                  ```
-                ''
-                lib.warnIf
-                (!hasPlugin) "Skipping \"${name}\" plugin"
-                hasPlugin);
-          findPackages = builtins.map
-            ({ name, version }:
-              let
-                plugin = plugins.${name};
-              in
-              lib.throwIf (!plugin.hasVersion { inherit system version; })
-                ''
-                  Plugin "${name}" does not provide version "${version}", try
-                  updating the plugin's input:
+                    ```
+                    <asdf2nix>.lib.packagesFromVersionsFile {
+                      plugins = { ... };
+                      skipMissingPlugins = true;
+                      ...
+                    };
+                    ```
+                  ''
+                  lib.warnIf
+                  (!hasPlugin) "Skipping \"${name}\" plugin"
+                  hasPlugin);
+            findPackages = builtins.map
+              ({ name, version }:
+                let
+                  plugin = plugins.${name};
+                in
+                lib.throwIf (!plugin.hasVersion { inherit system version; })
+                  ''
+                    Plugin "${name}" does not provide version "${version}", try
+                    updating the plugin's input:
 
-                  ```
-                  > nix flake lock --update-input <asdf2nix-${name}>
-                  ```
-                ''
-                {
-                  inherit name;
-                  value = plugin.packageFromVersion { inherit system version; };
-                }
-            );
-        in
-        builtins.listToAttrs
-          (findPackages
-            (filterPlugins
-              (parseVersions
-                (removeComments
-                  (fileLines versionsFile)))));
+                    ```
+                    > nix flake lock --update-input <asdf2nix-${name}>
+                    ```
+                  ''
+                  {
+                    inherit name;
+                    value = plugin.packageFromVersion { inherit system version; };
+                  }
+              );
+          in
+          builtins.listToAttrs
+            (findPackages
+              (filterPlugins
+                (parseVersions
+                  (removeComments
+                    (fileLines versionsFile)))));
 
-      templates = {
-        default = {
-          description = "Install Nix packages from asdf versions file";
-          path = ./templates/default;
+        templates = {
+          default = {
+            description = "Install Nix packages from asdf versions file";
+            path = ./templates/default;
+          };
+          devenv = {
+            description = "Integration with devenv";
+            path = ./templates/devenv;
+          };
         };
-        devenv = {
-          description = "Integration with devenv";
-          path = ./templates/devenv;
-        };
-      };
-    };
+      }
+    );
 }
