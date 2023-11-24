af7776e7dd1c2b5ee8e7f4f1aaad322bc0e932da
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-11-24 03:35:19 -0500

    Increase python test coverage

--- tests/plugins/python/lib_test.nix
+++ tests/plugins/python/lib_test.nix
@@ -3,8 +3,18 @@ let
 in
 [
   {
-    name = "When the version exists";
-    actual = (lib.packageFromVersion { version = "3.12.0"; }).version;
-    expected = "3.12.0";
+    name = "When the version exists returns true";
+    actual = lib.hasVersion { version = "3.12.0"; };
+    expected = true;
+  }
+  {
+    name = "When the version does not exist returns false";
+    actual = lib.hasVersion { version = "0.0.0"; };
+    expected = false;
+  }
+  {
+    name = "When the version exists returns a package";
+    actual = (lib.packageFromVersion { version = "3.12.0"; }).name;
+    expected = "python3-3.12.0";
   }
 ]
